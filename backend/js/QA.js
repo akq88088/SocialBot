6bbf7dbba40b603a0abee26dd64b059c50226f52
alex07366032 <07366032@me.mcu.edu.tw>
2019-09-05 01:07:49 +0800

    add buttun click function

--- backend/js/QA.js
+++ backend/js/QA.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 	$('#sidebar > .worker').on('click', function(){
-		$(this).parent().find('.worker').removeClass('active');
-		$(this).toggleClass('active');
+		// $(this).parent().find('.worker').removeClass('active');
+		// $(this).toggleClass('active');
+		window.location.href = $(this).attr('id') + '.html';
 	});
+
 
 	$('#add_project').on('click', function(){
 		window.location.href = 'dashboard.html';
