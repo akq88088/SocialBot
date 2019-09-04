4a5f8bf0702a06641d23177258a0a15830c40515
alex07366032 <07366032@me.mcu.edu.tw>
2019-09-05 00:50:44 +0800

    fix backend css and js path

---
+++ backend/js/QA.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('#sidebar > .worker').on('click', function(){
+		$(this).parent().find('.worker').removeClass('active');
+		$(this).toggleClass('active');
+	});
+
+	$('#add_project').on('click', function(){
+		window.location.href = 'dashboard.html';
+	});
+
+	$('.c-project').on('click', function(){
+		window.location.href = 'dashboard.html';
+	});
+});
