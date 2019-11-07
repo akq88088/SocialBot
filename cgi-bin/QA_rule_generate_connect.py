75a5fa462644ce825a168d29d64f54a50c24eba7
akq88088 <54851652+akq88088@users.noreply.github.com>
2019-11-07 11:22:40 +0800

    QA train revise

--- cgi-bin/QA_rule_generate_connect.py
+++ cgi-bin/QA_rule_generate_connect.py
@@ -7,11 +7,11 @@ import numpy as np
 import json
 from module.QA_train import QA_train
 
-# parameter = cgi.FieldStorage()
-# owner = parameter.getvalue('owner')
-# p_name = parameter.getvalue('p_name')
-owner = "c2525a7f58ae3776070e44c106c48e15"
-p_name = "tt"
+parameter = cgi.FieldStorage()
+owner = parameter.getvalue('owner')
+p_name = parameter.getvalue('p_name')
+# owner = "c2525a7f58ae3776070e44c106c48e15"
+# p_name = "tt"
 QA_train = QA_train(owner,p_name)
 QA_train.read_data_generate_rule_main()
 print("Content-type:text/html") #必須
