e0488ee4fc5695a23b38a7a2e359ad6d08027bbe
LIANG-TING-CHEN <54851652+LIANG-TING-CHEN@users.noreply.github.com>
2019-12-22 20:20:36 +0800

    add sample download and document page
    1222

---
+++ backend/sample_download.php
@@ -0,0 +1,9 @@
+<html>
+　<head>
+　<title></title>
+　</head>
+　<body>
+    <div><a href='../cgi-bin/module/QA_data/出題問答.csv' download='出題問答.csv'>出題問答範例檔案下載</a></div>
+    <div><a href='../cgi-bin/module/QA_data/出題問答保留字字典.csv' download='出題問答保留字字典.csv'>出題問答保留字字典範例檔案下載</a></div>
+  </body>
+</html>
